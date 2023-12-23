7ce17b0f50009b87f2f1ba8ee4c89a1b849c3854
maxonofficial <marshalmaxon33@gmail.com>
2023-12-23 09:19:12 +0530

    Updated the boilerplate code

--- app/src/main/java/com/example/travelphotoapp/MainActivity.kt
+++ app/src/main/java/com/example/travelphotoapp/MainActivity.kt
@@ -11,48 +11,39 @@ import android.widget.TextView
 import androidx.constraintlayout.widget.ConstraintSet
 
 class MainActivity : AppCompatActivity() {
-    var currentImage = 0
+    private var currentImage = 0
 
-    lateinit  var image : ImageView
+    private lateinit  var image : ImageView
 
-    val touristPlaces = arrayOf("The India Gate", "The Lotus Temple", "Taj Mahal", "Qutub Minar", "The Humayun’s Tomb" )
-
+    private val touristPlaces = arrayOf("The India Gate", "The Lotus Temple", "Taj Mahal", "Qutub Minar", "The Humayun’s Tomb" )
+    private val drawableIds = arrayOf(
+        R.drawable.pic0, R.drawable.pic1, R.drawable.pic2, R.drawable.pic3, R.drawable.pic4
+    )
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        var placeName: TextView = findViewById(R.id.tvPlace)
+        val placeName: TextView = findViewById(R.id.tvPlace)
 
         val prev = findViewById<ImageButton>(R.id.btnPrev)
         val next = findViewById<ImageButton>(R.id.btnNext)
 
+        image = findViewById(R.id.pic)
+        image.setImageResource(drawableIds[currentImage])
+        placeName.text = touristPlaces[currentImage]
+
         prev.setOnClickListener {
             // get the previous Image
-            val idCurrentImageString = "pic$currentImage"
-            val idCurrentImageInt = this.resources.getIdentifier(idCurrentImageString,"id",packageName)
-            image = findViewById(idCurrentImageInt)
-            image.alpha = 0f
-            currentImage = (5 + currentImage - 1) % 5
+            currentImage = (5 + currentImage + 1) % 5
+            image.setImageResource(drawableIds[currentImage])
+            placeName.text = touristPlaces[currentImage]
 
-            val idPrevImageString = "pic$currentImage"
-            val idPrevImageInt = this.resources.getIdentifier(idPrevImageString,"id",packageName)
-            image = findViewById(idPrevImageInt)
-            image.alpha = 1f
-            placeName.setText(touristPlaces[currentImage])
         }
 
         next.setOnClickListener {
             // get the next Image
-            val idCurrentImageString = "pic$currentImage"
-            val idCurrentImageInt = this.resources.getIdentifier(idCurrentImageString,"id",packageName)
-            image = findViewById(idCurrentImageInt)
-            image.alpha = 0f
             currentImage = (5 + currentImage + 1) % 5
-
-            val idPrevImageString = "pic$currentImage"
-            val idPrevImageInt = this.resources.getIdentifier(idPrevImageString,"id",packageName)
-            image = findViewById(idPrevImageInt)
-            image.alpha = 1f
-            placeName.setText(touristPlaces[currentImage])
+            image.setImageResource(drawableIds[currentImage])
+            placeName.text = touristPlaces[currentImage]
 
         }
     }
